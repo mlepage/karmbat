bf6c2c527bc3e3fe9f97b9de83a90694d3156658
Marc Lepage <mlepage@antimeta.com>
2015-01-24 16:41:25 -0500

    Make arena mirror itself

---
+++ Assets/Scripts/ArenaMaker.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArenaMaker : MonoBehaviour {
+
+	void Start () {
+		// Clone arena
+		GameObject clone = GameObject.Instantiate(gameObject) as GameObject;
+
+		// Reflect it along x
+		Vector3 scale = clone.transform.localScale;
+		scale.x = -scale.x;
+		clone.transform.localScale = scale;
+
+		// Remove this script from the clone
+		Destroy(clone.GetComponent<ArenaMaker>());
+	}
+
+}
