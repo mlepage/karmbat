288091ac51f47d76b666722f7824d27dd0237908
Marc Lepage <mlepage@antimeta.com>
2015-01-24 17:08:26 -0500

    Prevent tanks from moving in center of arena

--- Assets/Scripts/TankControl.cs
+++ Assets/Scripts/TankControl.cs
@@ -25,7 +25,12 @@ public class TankControl : MonoBehaviour {
 				float rot = Mathf.Atan2(move.y, move.x) * Mathf.Rad2Deg;
 				body.transform.rotation = Quaternion.Euler(0f, 0f, rot - 90f);
 				// Move
+				Vector3 position = body.transform.position;
 				body.transform.Translate(move * speed, Space.World);
+				if (body.transform.position.magnitude < 1f) {
+					// Revert position if too close to center
+					body.transform.position = position;
+				}
 			}
 		}
 
