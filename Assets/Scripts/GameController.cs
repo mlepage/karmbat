5343b07e6714add386d6854b8dc0da79bcd37f8e
Marc Lepage <mlepage@antimeta.com>
2015-01-24 23:46:01 -0500

    Make shots kill tanks

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -22,10 +22,16 @@ public class GameController : MonoBehaviour {
 	public void Shoot () {
 		GameObject shot1 = Instantiate(tankShot, turret1.transform.position, turret1.transform.rotation) as GameObject;
 		(shot1.renderer as SpriteRenderer).color = (turret1.renderer as SpriteRenderer).color;
+		shot1.tag = tank1.tag;
+		ShotCollider shotCollider1 = shot1.AddComponent<ShotCollider>();
+		shotCollider1.targetTag = tank2.tag;
 		shot1.AddComponent<ShotControl>();
 
 		GameObject shot2 = Instantiate(tankShot, turret2.transform.position, turret2.transform.rotation) as GameObject;
 		(shot2.renderer as SpriteRenderer).color = (turret2.renderer as SpriteRenderer).color;
+		shot2.tag = tank2.tag;
+		ShotCollider shotCollider2 = shot2.AddComponent<ShotCollider>();
+		shotCollider2.targetTag = tank1.tag;
 		Doppelganger dop = shot2.AddComponent<Doppelganger>();
 		dop.target = shot1;
 	}
---
+++ Assets/Scripts/ShotCollider.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShotCollider : MonoBehaviour {
+
+	public string targetTag;
+
+	void Update () {
+		Collider2D other = Physics2D.OverlapCircle(transform.position, 0.15f);
+		if (other && other.CompareTag(targetTag)) {
+			Destroy(gameObject);
+			Destroy(other.gameObject);
+		}
+	}
+}
